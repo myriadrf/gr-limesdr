b9a36901354666edaa09f478b802a1a2cc568dbf
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2018-04-25 22:03:37 +0300

    Remove cmake generated files from source

--- cmake/docs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/osvaldas/gitREPOS/gr-limesdr/docs
-# Build directory: /home/osvaldas/gitREPOS/gr-limesdr/cmake/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake/swig/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/osvaldas/gitREPOS/gr-limesdr/swig
-# Build directory: /home/osvaldas/gitREPOS/gr-limesdr/cmake/swig
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
