7a7efb01d2155b2d12246445f377b6e3cfde98a7
Heath Jones <hj1980@gmail.com>
2019-08-24 17:45:01 +1000

    Implement full limesdr source

--- lib/source_impl.h
+++ lib/source_impl.h
@@ -25,6 +25,8 @@
 
 #include "device_handler.h"
 
+static const pmt::pmt_t TIME_TAG = pmt::string_to_symbol("rx_time");
+
 namespace gr {
   namespace limesdr {
 
@@ -53,21 +55,48 @@ namespace gr {
         void print_stream_stats(lms_stream_status_t status);
 
         void add_time_tag(int channel, lms_stream_meta_t meta);
-        // Nothing to declare in this block.
 
-     public:
-      source_impl(std::string serial, int channel_mode, const std::string& filename);
-      ~source_impl();
+      public:
+        source_impl(std::string serial, int channel_mode, const std::string& filename);
+        ~source_impl();
 
-      // Where all the action really happens
-      int work(
-              int noutput_items,
-              gr_vector_const_void_star &input_items,
-              gr_vector_void_star &output_items
-      );
+        bool start(void);
 
-      inline gr::io_signature::sptr args_to_io_signature(int channel_mode);
+        bool stop(void);
 
+        // Where all the action really happens
+        int work(
+            int noutput_items,
+            gr_vector_const_void_star &input_items,
+            gr_vector_void_star &output_items
+            );
+
+        inline gr::io_signature::sptr args_to_io_signature(int channel_mode);
+
+        void init_stream(int device_number, int channel);
+        void release_stream(int device_number, lms_stream_t *stream);
+
+        double set_center_freq(double freq, size_t chan = 0);
+
+        void set_antenna(int antenna, int channel = 0);
+
+        void set_nco(float nco_freq, int channel = 0);
+
+        double set_bandwidth(double analog_bandw, int channel = 0);
+
+        void set_digital_filter(double digital_bandw, int channel = 0);
+
+        unsigned set_gain(unsigned gain_dB, int channel = 0);
+
+        double set_sample_rate(double rate);
+
+        void set_oversampling(int oversample);
+
+        void set_buffer_size(uint32_t size);
+
+        void calibrate(double bandw, int channel = 0);
+
+        void set_tcxo_dac(uint16_t dacVal = 125);
     };
 
   } // namespace limesdr
